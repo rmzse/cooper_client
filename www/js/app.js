e272ca925f022a53d903027f9ee09a5d12170d13
rmzse <rodrigo@therawdelicious.com>
2017-01-21 18:14:36 +0100

    add route to test view, app working

--- www/js/app.js
+++ www/js/app.js
@@ -39,7 +39,18 @@ angular.module('starter', ['ionic', 'starter.controllers'])
           templateUrl: 'templates/about/about.html'
         }
       }
-    });
+    })
+
+  .state('app.test', {
+    url: '/test',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/test/test.html',
+        controller: 'TestController'
+      }
+    }
+  });
+
 
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/about');

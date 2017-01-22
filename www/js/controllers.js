ed7e07eb685b893ce777165018535eda7e3e9ad1
rmzse <rodrigo@therawdelicious.com>
2017-01-22 17:19:46 +0100

    add login method to controller, app responding

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -31,13 +31,15 @@ angular.module('starter.controllers', [])
 
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
+    $auth.submitLogin($scope.loginData)
+      .then(function (resp) {
+        // handle success response
+        $scope.closeLogin();
+      })
+      .catch(function (error) {
+        // handle error response
+        $scope.errorMessage = error;
+      });
   };
 })
 
